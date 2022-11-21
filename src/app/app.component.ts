05175317096fd7d36d59d57c66438da03582ff65
why520crazy <why520crazy@163.com>
2022-11-21 09:47:24 +0800

    feat: add Hello Angular Relearning!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-relearning';
+  title = 'Hello Angular Relearning!';
 }
