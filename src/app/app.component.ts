c46e7699a5aa345723caff07f7a497f4a9960abb
why520crazy <why520crazy@163.com>
2022-11-22 09:30:46 +0800

    feat: improve forms and nav use material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatTabsModule],
 })
 export class AppComponent {
   title = 'Hello Angular Relearning!';
+
+  current = '';
+
+  links = [
+    {
+      link: 'ng-if',
+      text: 'NgIf',
+    },
+    {
+      link: 'event-binding',
+      text: 'EventBinding',
+    },
+    {
+      link: 'ng-for',
+      text: 'NgFor',
+    },
+    {
+      link: 'forms',
+      text: 'Forms',
+    },
+  ];
+
+  activeLink = '';
 
   constructor() {}
 }
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatInputModule],
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss'],
 })
