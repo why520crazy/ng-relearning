0d2b36c2ce992de50bd87510b33b1816f359bd37
why520crazy <why520crazy@163.com>
2022-11-23 08:50:29 +0800

    feat: refactor http-client and remove todo service

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Todo, TodoService } from '../todo.service';
+import { HttpClient } from '@angular/common/http';
+
+export interface Todo {
+  id?: string;
+  title: string;
+  created_by?: string;
+}
 
 @Component({
   selector: 'app-ng-for',
@@ -9,12 +15,16 @@ import { Todo, TodoService } from '../todo.service';
   templateUrl: './ng-for.component.html',
   styleUrls: ['./ng-for.component.scss'],
 })
-export class NgForComponent {
-  items!: Todo[];
+export class NgForComponent implements OnInit {
+  todos!: Todo[];
 
-  constructor(private todoService: TodoService) {
-    this.todoService.fetchTodos().subscribe((items) => {
-      this.items = items;
-    });
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Todo[]>('https://62f70d4273b79d015352b5e5.mockapi.io/items')
+      .subscribe((items) => {
+        this.todos = items;
+      });
   }
 }
